a406ac68eba013282eb2c5489dad8dee412ac2b6
hyojin kwon <raccoonhj33@gmail.com>
2020-05-31 15:04:09 +0900

    200531 bs4 - find_all, find

--- python_crawl/section5-1.py
+++ python_crawl/section5-1.py
@@ -12,7 +12,7 @@ html = '''
         <p class="story">Once upon a time there were three little sisters.
             <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>
             <a href="http://example.com/lacie" class="sister" id="link2">Laice</a>
-            <a data-io="link3" href="http://example.com/little" class="sister" id="link3">Title</a>
+            <a data-io="link3" href="http://example.com/little" class="brother" id="link3">Title</a>
         </p class="story">
         <p>
             story ...
@@ -43,4 +43,23 @@ p3 = p2.next_sibling.next_sibling
 for v in p2.next_element:
     # print('v >> ', v)
     pass
+
+soup2 = BeautifulSoup(html, 'html.parser')
+link1 = soup.find_all('a', limit=2)
+# print('link1 >> ', link1)
+link2 = soup.find_all('a', class_='sister')
+# link2 = soup.find_all('a', string=['Elsie', 'Title'])
+# print('link2 >> ', link2)
+
+for t in link2:
+    # print('t >> ', t)
+    pass
+
+link3 = soup.find('a')
+# print('link3 >> ', link3)
+# print('link3.string >> ', link3.string)
+# print('link3.text >> ', link3.text)
+link4 = soup.find('a', {"class": "brother", "data-io": "link3"})
+# print('link4 >> ', link4)
+
 
